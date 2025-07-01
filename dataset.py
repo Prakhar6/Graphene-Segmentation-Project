1114b0777da3e92f53a0e52f9ebac45d5e695feb
Leo Lu <leo.hz.lu@gmail.com>
2025-07-01 12:28:33 -0400

    added mask matching/labeling

--- dataset.py
+++ dataset.py
@@ -9,16 +9,18 @@ class GrapheneSegmentationDataset(Dataset):
     def __init__(self, image_dir, mask_dir, transform=None):
         self.image_dir = image_dir
         self.mask_dir = mask_dir
-        self.images = sorted(os.listdir(image_dir))
-        self.masks = sorted(os.listdir(mask_dir))
+        self.images = sorted([f for f in os.listdir(image_dir) if f.endswith(('.png', '.jpg', '.jpeg'))])
+        self.masks = sorted([f for f in os.listdir(mask_dir) if f.endswith(('.png', '.jpg', '.jpeg'))])
         self.transform = transform
 
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.image_dir, self.images[idx])
-        mask_path = os.path.join(self.mask_dir, self.masks[idx])
+        img_name = self.images[idx]
+        mask_name = img_name.replace('.jpg', '_mask.png').replace('.png', '_mask.png')
+        img_path = os.path.join(self.image_dir, img_name)
+        mask_path = os.path.join(self.mask_dir, mask_name)
 
         image = Image.open(img_path).convert("RGB")
         mask = Image.open(mask_path)
